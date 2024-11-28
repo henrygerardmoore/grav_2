42aa83eaec66671e717324efbc232fb94fad686f
Henry Moore <henrygerardmoore@gmail.com>
2024-11-27 17:53:05 -0700

    re-arrange some functions

--- src/systems.rs
+++ src/systems.rs
@@ -3,14 +3,10 @@ use bevy::{
     input::mouse::{MouseMotion, MouseWheel},
     math::NormedVectorSpace,
     prelude::*,
-    render::{
-        render_asset::RenderAssetUsages,
-        render_resource::{Extent3d, TextureDimension, TextureFormat},
-    },
     window::PrimaryWindow,
 };
 
-use crate::components::{Body, HelpText, HelpUI, Position, SpawnText, SpawnUI, Velocity};
+use crate::{components::{Body, HelpText, HelpUI, Position, SpawnText, SpawnUI, Velocity}, helpers::{body_bundle, uv_debug_texture}};
 use crate::helpers::{get_mass, get_radius};
 use crate::resources::{BodySpawningOptions, SpawnSelectionMode, SphereInfo, TimePaused, TimeRate};
 
@@ -410,35 +406,6 @@ pub fn exit_system(keys: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExi
     }
 }
 
-// from 3d_shapes.rs bevy example
-pub fn uv_debug_texture() -> Image {
-    const TEXTURE_SIZE: usize = 8;
-
-    let mut palette: [u8; 32] = [
-        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
-        198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
-    ];
-
-    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
-    for y in 0..TEXTURE_SIZE {
-        let offset = TEXTURE_SIZE * y * 4;
-        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
-        palette.rotate_right(4);
-    }
-
-    Image::new_fill(
-        Extent3d {
-            width: TEXTURE_SIZE as u32,
-            height: TEXTURE_SIZE as u32,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &texture_data,
-        TextureFormat::Rgba8UnormSrgb,
-        RenderAssetUsages::RENDER_WORLD,
-    )
-}
-
 pub fn create_sphere_info(
     mut sphere_info: ResMut<SphereInfo>,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -452,29 +419,6 @@ pub fn create_sphere_info(
     });
     sphere_info.0 = mesh_handle;
     sphere_info.1 = material_handle;
-}
-
-pub fn body_bundle(
-    mass: f32,
-    position: Vec3,
-    velocity: Vec3,
-    sphere_info: &Res<SphereInfo>,
-) -> impl Bundle {
-    // get or add the mesh handle
-    let mesh_handle = sphere_info.0.clone();
-
-    // get or add the material handle
-    let material_handle = sphere_info.1.clone();
-    (
-        Body { mass },
-        Position(position),
-        Velocity(velocity),
-        PbrBundle {
-            mesh: mesh_handle,
-            material: material_handle,
-            ..default()
-        },
-    )
 }
 
 pub fn initial_spawn(mut commands: Commands, sphere_info: Res<SphereInfo>) {
